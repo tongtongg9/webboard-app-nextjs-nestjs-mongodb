a39c7df43b609447abb159197f52859d2e34259a
tongtongg9 <kampon.lamyai@gmail.com>
2024-08-15 14:49:45 +0700

    Update global validation pipe for input data

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,8 +1,30 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ConfigService } from '@nestjs/config';
+import { BadRequestException, ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
     const app = await NestFactory.create(AppModule);
-    await app.listen(3000);
+
+    const configService = app.get(ConfigService);
+
+    // Set up global validation pipe
+    app.useGlobalPipes(
+        new ValidationPipe({
+            whitelist: true, // ตัด properties ของข้อมูลที่ส่งเข้ามาที่ไม่ได้นิยามไว้ใน dto ออกไป
+            forbidNonWhitelisted: true, // ตัวเลือกนี้จะทำงานคู่กับ whitelist โดยหากตั้งค่าเป็น true จะทำให้เกิด error ในกรณีนี้มี properties ใดที่ไม่ได้อยู่ใน whitelist ส่งเข้ามา
+            transform: true, // ตัวเลือกนี้ทำให้เกิดการแปลงชนิดข้อมูลอัตโนมัติ ในข้อมูลจากภายนอกให้ตรงกับชนิดที่นิยามไว้ใน DTO
+            exceptionFactory: (errors) => {
+                // ตัวเลือกนี้ทำให้สามารถกำหนดรูปแบบของ error response เมื่อการตรวจ validation ล้มเหลวได้
+                const messages = errors.map((error) => ({
+                    field: error.property,
+                    message: Object.values(error.constraints).join('. ') + '.',
+                }));
+                return new BadRequestException({ errors: messages });
+            },
+        }),
+    );
+
+    await app.listen(configService.get('port'));
 }
 bootstrap();
